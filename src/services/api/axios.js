01cd9254144aa747e0a4e93d8ef112d93743c674
Imad gannoun <75321420+ImadBen96@users.noreply.github.com>
2024-05-03 15:07:24 +0200

    Update axios.js

--- src/services/api/axios.js
+++ src/services/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosClient = axios.create({
-    baseURL:  "http://localhost:8000",
+    baseURL:  "https://simpleecommercestripe.imadgannoun.com",
     timeout:60000,
     withCredentials: true,
     xsrfCookieName: "XSRF-TOKEN",
